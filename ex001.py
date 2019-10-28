17e6b61bdb4ce1aeafb0afba86cf5bbbc7c13a65
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2019-10-28 01:43:56 -0300

    add exe script
    func python3

---
+++ ex001.py
@@ -0,0 +1,8 @@
+
+#não é uma boa prática usar espaços entre função e parametro
+#EX-->print('olá mundo')
+
+
+#No python 3 sar formatação usando {} para sub
+nome = input('Qual é o seu nome?')
+print ('Muito prazer em te conhecer {}!'.format(nome))
