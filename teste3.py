1d5d05acad6ed8d8962ecc169d60e86188da89e5
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2019-10-27 23:32:45 -0300

    script3
    Add func script

---
+++ teste3.py
@@ -0,0 +1,5 @@
+Dia = input ('Que dia você nasceu?')
+Mes = input ('Qual o mês do seu nascimento?')
+Ano = input ('Que ano voce nasceu?')
+
+print ('Vc nasceu no dia', Dia, 'de', Mes, 'de', Ano, 'Certo?')
