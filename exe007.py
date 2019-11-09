2154d3023827cea8e4f785ea9f218bee96df27aa
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2019-11-09 02:55:00 -0300

    Add file
    exe 007

---
+++ exe007.py
@@ -0,0 +1,12 @@
+#Funçao que diz se valr na Var são letras. 
+#Thursday, November7 2019
+'''
+n = input('Digite algo: ')
+print(n.isalpha())
+'''
+
+# Crie um Programa que leia dois numeros e mostre a soma entre eles.
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Dig outro numero: '))
+s = n1 + n2
+print (f'A soma entre, {n1} de {n2} é {s}')
