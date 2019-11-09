b4048c9a8f8277da58989bb293abb91ced7ca6d5
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2019-11-09 02:53:45 -0300

    add file
     Tipos primitivos de daddos

---
+++ tipos.py
@@ -0,0 +1,21 @@
+#Tipos Primitivos e Saida de Dados
+
+#int - Numeros inteiros 6,9,34, ...
+#float - Numeros com pontos flutuantes 55.7
+#bool - Logicos e Booleanos True False
+#str - Caracteres 'Palavra'
+
+#Utilizar a funçao format
+
+#print ( 'A soma vale',)
+
+#print ('A soma vale{}'.format(s))
+
+
+#Soma dois numeros e soma mostrando o resultado
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro: '))
+s = n1 + n2
+print('A Soma entre {} e {} vale {}' .format(n1, n2, s))
+# print('A soma entre n1 e n2 vale', s)
