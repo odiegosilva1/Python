60d66b5fa40c7864715da1014b58718701a410c1
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2019-11-29 01:44:17 -0300

    Update name

---
+++ Hello_World.py
@@ -0,0 +1,5 @@
+
+# Fuction Print Hello World!
+
+nome = input('Qual o seu nome?')
+print ('Olá', nome, 'seja bem vindo!')
--- teste1.py
+++
@@ -1,3 +0,0 @@
-
-nome = input('Qual o seu nome?')
-print ('Olá', nome, 'seja bem vindo!')
