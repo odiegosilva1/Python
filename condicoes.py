6fed44f0c53d35e8856ecd1d9bad3af9c4bd99bd
Diego Silva <41221742+odiegosilva1@users.noreply.github.com>
2019-11-29 01:52:41 -0300

    Add files via upload

---
+++ condicoes.py
@@ -0,0 +1,9 @@
+# Descobindo tipos de variável 
+
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(a))
+print('Só tem espaços?: ', a.isspace())
+print('É um numero?: ', a.isnumeric())
+print('É alfabético?: ', a.isalpha())
+print('É alfanumerico?: ', a.isalnum())
+
