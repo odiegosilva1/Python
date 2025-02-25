c65a8e900252c715e187f18ba8471897d05b3591
odiegosilva1 <odiegosilvadev@gmail.com>
2025-02-25 15:19:10 -0300

    add-exe01.py

---
+++ exe02.py
@@ -0,0 +1,13 @@
+# Para cada situação abaixo, escreva um trecho de código para solicitar que o usuário digite tais dados e eles sejam armazenados na memória de forma correta (em relação ao tipo do dado):
+
+# solicitar o nome do usuário
+# solicitar a idade do usuário
+# Ssolicitar o peso do usuário
+
+name = input("Digite o seu nome: ")
+age = int(input("Diegite a sua Idade: "))
+weight = float(input("Qual é o seu peso: "))
+
+print(name)
+print(age)
+print(weight)
