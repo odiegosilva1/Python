39370782bcbc3176fbe1479e5e7ea0ac7e8affb5
odiegosilva1 <odiegosilvadev@gmail.com>
2025-02-26 14:48:52 -0300

    add: exe05 e 06

---
+++ exe05.py
@@ -0,0 +1,5 @@
+# Concatenação
+
+name = input("Diego o Seu Nome: ")
+age =input("Qual é a sua idade ?: ")
+print("Olá",name,"você tem", age, "anos de vida, seja bem-vindo!")
---
+++ exe06.py
@@ -0,0 +1,7 @@
+#Pergunatndo noem e altura
+
+name = input("Digite o seu nome: ")
+print("Seja bem-vindo",name)
+height = float(input("Qual é a sua altura: "))
+print("Você tem", height,"de altura")
+print("Pronto vá embora por favor!")
