dd8c4d2101bdd5c68792e2967265546a0007f3f4
odiegosilva1 <odiegosilvadev@gmail.com>
2025-02-26 14:33:39 -0300

    Update exe03.py

--- exe03.py
+++ exe03.py
@@ -4,7 +4,7 @@ nota1 = float(input("Diegote a pimeira nota: "))
 nota2 = float(input("Diegite a seguna nota: "))
 nota3 = float(input("Digite a terceira nota: "))
 
-operacao = (nota1 + nota2 + nota3 / 3)
+operacao = (nota1 + nota2 + nota3) / 3
 
 print("Sua note final é", operacao)
 
