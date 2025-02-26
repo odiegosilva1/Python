ffdb3422d2bbf9a93ffe760af6e1cbf40a4bc7ae
odiegosilva1 <odiegosilvadev@gmail.com>
2025-02-26 14:09:17 -0300

    add: exe04.py

---
+++ exe03.py
@@ -0,0 +1,10 @@
+# Soma Média
+
+nota1 = float(input("Diegote a pimeira nota: "))
+nota2 = float(input("Diegite a seguna nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+operacao = (nota1 + nota2 + nota3 / 3)
+
+print("Sua note final é", operacao)
+
---
+++ exe04.py
@@ -0,0 +1,7 @@
+# Mostra Desconto
+
+price = float(input("Digite o valor do produto:"))
+discount = price * 0.1
+
+print("O valor do desconto é :", discount)
+print("Valor do desconto aplicádo: ",(price-discount))
