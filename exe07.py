369d04efadfa7a7a942827efed88763bc210082a
odiegosilva1 <odiegosilvadev@gmail.com>
2025-03-02 10:11:24 -0300

    Create exe07.py

---
+++ exe07.py
@@ -0,0 +1,9 @@
+# Endereço 
+
+print("***Cadastro de usuário***")
+street = input("Qula é o nome da sua rua?: ")
+number = int(input("Qual é número da sua casa?: ")) 
+zipCode = int(input("Qual é o seu CEP ?: "))
+city = input("Qual é a Sua Cidade ?: ")
+
+print("Você mora na cidade de", city, "o nome da rua é", street ,"no número", number, "com o CEP", zipCode)
