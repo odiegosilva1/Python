f24d04be35a74419fa706c71adf938ae72c8a432
odiegosilva1 <odiegosilvadev@gmail.com>
2025-03-13 13:58:39 -0300

    change: main.py

--- GAME/main.py
+++
@@ -1,9 +0,0 @@
-import pygame
-from sprites import *
-from config import *
-import sys
-
-class Game:
-  def __init__(self):
-    pygame.init()
-    self.screen = pygame.display.set_mode((WIN_WIDTH, WIN_HECIGHT))
