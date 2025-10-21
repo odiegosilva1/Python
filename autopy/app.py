755ee9db168e8a65c6ebb4fcf341dd4a251c9880
odiegosilva1 <odiego2099@gmail.com>
2025-10-21 09:17:13 -0300

    update: readme

---
+++ autopy/app.py
@@ -0,0 +1,4 @@
+# Ler dados de qualquer planilha
+# Aitomatiza entrada de dados e planilhas usando Python.
+# Inseri daddos de qualquer fonte(word, excel, outros sistemas)
+# Envia Informações daddospor e-mail, whatsApp, telegran e etc.
