a143b7307fcb866d21bd3bf345ca3ab01bfcebbf
Diego Silva <odiegosilvadev@gmail.com>
2026-03-08 19:41:21 -0300

    update-main.py

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+# Rora raiz
+@app.route('/')
+def index():
+    return('Olá mowdoo')
+
 if __name__ == '__main__':
-    app:run()
+    app.run()
