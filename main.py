586dc90ed1de18f5799bc405d2170bcd36dfd9bd
Diego Silva <odiegosilvadev@gmail.com>
2026-03-09 15:34:19 -0300

    create: database

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 
 app = Flask(__name__)
+app.config['SQLAMCHEMY_DATABASE_URI']
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-
-@app.route('/formulario', methods=['GET', 'POST'])
-def formulario():
-   if request.method == "POST": 
-    nome = request.form['nomeForm']
-    email = request.form['emailForm']
-    print(f"Nome: {nome}")
-    print(f"email: {email}")
-    return render_template('formulario.html')
 
 
 if __name__ == '__main__':
