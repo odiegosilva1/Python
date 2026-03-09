1f8b0fa55c49d0f3f06eac61ed3dfd22bf94a5dc
Diego Silva <odiegosilvadev@gmail.com>
2026-03-09 14:15:51 -0300

    add style.css

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -9,7 +9,12 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/formulario', methods=['GET', 'POST'])
+def formulario():
+    nome = request.form['nomeForm']
+    email = request.form['emailForm']
 
+    return render_template('formulario.html')
 
 
 if __name__ == '__main__':
