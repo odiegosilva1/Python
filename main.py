60701216c52fec1027003758c798723b580d5e56
Diego Silva <odiegosilvadev@gmail.com>
2026-03-11 16:24:12 -0300

    add: routes-base

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 #Rotas e metodos aceitos
 @app.route('/', methods=['GET'])
 def ola_mundo():
-    return "Aee"
+    return render_template('index.html')
 
 @app.route("/sobre")
 def pagina_sobre():
-    return "Sobre"
+    return render_template('/sobre.html')
 
 
 
