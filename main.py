d2baa9ef43e187eaab0bd50c541c4b1f6518ee2c
Diego Silva <odiegosilvadev@gmail.com>
2026-03-11 15:42:03 -0300

    rebase-base.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from markupsafe import escape
+from flask import Flask
+from db import db
+from models import Usuario
 
 app = Flask(__name__)
-app.config['SQLAMCHEMY_DATABASE_URI']
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # CORREÇÃO 2: Formato correto da URI
+
+# Inicializa o db com o app
+db.init_app(app)
 
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Cria as tabelas
+        print("Banco de dados criado com sucesso!")
     app.run(debug=True)
