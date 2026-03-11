5b7c76acce4778b3557ef0c7e58495e87583fc64
Diego Silva <odiegosilvadev@gmail.com>
2026-03-11 16:02:29 -0300

    remove: db.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from flask import Flask
-from db import db
-from models import Usuario
 
 app = Flask(__name__)
 
+#Rotas e metodos aceitos
+@app.route('/', methods=['GET'])
+def ola_mundo():
+    return "Aee"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # CORREÇÃO 2: Formato correto da URI
-
-# Inicializa o db com o app
-db.init_app(app)
-
+@app.route("/sobre")
+def pagina_sobre():
+    return "Sobre"
 
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Cria as tabelas
-        print("Banco de dados criado com sucesso!")
-    app.run(debug=True)
+
+
+app.run(debug=True)
--- models.py
+++
@@ -1,8 +0,0 @@
-from db import db
-
-class Usuario(db.Model):
-    __tablename__= 'usuarios'
-
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(30), unique=True)
-    senha = db.Column(db.String())
